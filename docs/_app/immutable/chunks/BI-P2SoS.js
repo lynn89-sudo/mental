8918eb8d9781dc5e5b0af4eb965a79a4d4e47404
Lynn <luke87joseph@gmail.com>
2025-10-28 00:46:29 +0000

    Forgot .nojekyll for deployment

---
+++ docs/_app/immutable/chunks/BI-P2SoS.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_xlk8ig?.base??"/mental",a=globalThis.__sveltekit_xlk8ig?.assets??s??"";export{a,s as b};
--- docs/_app/immutable/chunks/C0QhAP3_.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1uh6s8m?.base??"/mental",a=globalThis.__sveltekit_1uh6s8m?.assets??s??"";export{a,s as b};
